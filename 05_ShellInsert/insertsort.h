b382996024a9e5865e1365bf3fb263cb9c0d1d84
Aleksey Syryh <asyryh@gmail.com>
2019-07-25 11:50:11 +0500

    Insert sort

---
+++ 05_ShellInsert/insertsort.h
@@ -0,0 +1,44 @@
+#pragma once
+
+//По рабоче-крестьянски
+template<class array>
+void insertSort(array &data) {
+    int n = data.size();
+    for (int i = 0; i < n; ++i) {
+        auto x = data[i];
+        int j = i - 1;
+        while (j >= 0 && data[j] > x) {
+            data[j + 1] = data[j];
+            --j;
+        }
+        data[j + 1] = x;
+    }
+}
+
+//Модно, стильно, молодежно, по STLному (у нас тут современный C++ или где?)
+template<class BidirectionalIt, class Compare>
+void insertSort(BidirectionalIt first, BidirectionalIt last, Compare comp) {
+    if (first == last) return;
+    for (auto it = std::next(first); it < last; ++it) {
+        auto x = *it;
+        auto j = std::prev(it);
+        bool moveNeeded = true;
+        while (comp(x, *j)) {
+            *std::next(j) = *j;
+            if (j == first) { //Пришлось чутка покостылить, ведь std::prev(first) - это UB.
+                moveNeeded = false;
+                *j = x;
+                break;
+            }
+            --j;
+        }
+        if (moveNeeded) {
+            *std::next(j) = x;
+        }
+    }
+}
+
+template<class BidirectionalIt>
+void insertSort(BidirectionalIt first, BidirectionalIt last) {
+    insertSort(first, last, std::less<>());
+}
--- 05_ShellInsert/main.cpp
+++ 05_ShellInsert/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "insertsort.h"
+#include <array>
+#include <algorithm>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::array<int, 5> data = {34, 24, 25, 53, 23};
+    insertsort(data.begin(), data.end());
+    std::for_each(data.begin(), data.end(), [](int x) { std::cout << x << " "; });
     return 0;
 }
