7abb747ce82afe22676ad293f95956eeb5367381
Aleksey Syryh <asyryh@gmail.com>
2019-07-30 23:55:17 +0500

    Откат ошибочной правки

--- 05_ShellInsert/main.cpp
+++ 05_ShellInsert/main.cpp
@@ -39,7 +39,7 @@ void test(size_t len) {
     for (int i = 0; i < len; ++i) {
         data[i] = rand();
     }
-
+    doTest(data);
     std::sort(data.begin(), data.end());
     for (int i = 0; i < 5; ++i) {
         size_t ix1 = std::rand() * len / RAND_MAX;
@@ -47,13 +47,14 @@ void test(size_t len) {
         std::swap(data[ix1], data[ix2]);
     }
     std::cout << len << " 5 elemets swaped" << std::endl;
-
+    doTest(data);
     for (int i = 0; i < len * 0.1; ++i) {
         size_t ix1 = std::rand() * len / RAND_MAX;
         size_t ix2 = std::rand() * len / RAND_MAX;
         std::swap(data[ix1], data[ix2]);
     }
     std::cout << len << " 10% elemets swaped" << std::endl;
+    doTest(data);
 }
 
 int main() {
