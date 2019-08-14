ab5574c2ab83ac84aa588e90fd0fbf5ea928de9e
Aleksey Syryh <asyryh@gmail.com>
2019-08-14 11:52:11 +0500

    Малый правый поворот

--- 08_AVLDekart/avl.cpp
+++ 08_AVLDekart/avl.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <vector>
 #include <cmath>
+#include <sstream>
 
 
 template<typename T>
@@ -51,8 +52,16 @@ struct tree {
     void checkBalance(std::shared_ptr<node<T>> node) {
         while (node) {
             if (node->updateLevel()) {
-                int left = node->left ? node->left->level:0;
-                int right = node->right ? node->right->level:0;             }
+                if (doTurns) {
+                    int left = node->left ? node->left->level : 0;
+                    int right = node->right ? node->right->level : 0;
+                    if (left - right >= 2) {
+                        if (node->left->left->level >= node->left->right->level) {
+                            smallRightTurn(node);
+                        }
+                    }
+                }
+
                 node = node->parent;
             } else {
                 break;
@@ -82,6 +91,19 @@ struct tree {
             }
         }
         checkBalance(nodeToIns);
+    }
+
+    void smallRightTurn(std::shared_ptr<node<T>> a) {
+        auto b = a->left;
+        auto c = b->right;
+        a->left = c;
+        b->right = a;
+        b->parent = a->parent;
+        a->parent = b;
+        c->parent = a;
+        if (a == root) {
+            root = b;
+        }
     }
 
     std::shared_ptr<node<T>> find(T item) {
@@ -130,62 +152,118 @@ struct tree {
         item->right.reset();
     }
 
-    void print() {
+    std::string print() {
+        std::ostringstream oss;
         if (!root)
-            return;
+            return oss.str();
         std::vector<std::shared_ptr<node<T>>> currentLevel;
         currentLevel.push_back(root);
         size_t level = root->level;
         while (std::any_of(currentLevel.begin(), currentLevel.end(), [](auto ptr) { return ptr; })) {
 
             for (size_t offset = 0; offset < std::pow(2, level) - 1; ++offset) {
-                std::cout << ' ';
+                oss << ' ';
             }
             std::vector<std::shared_ptr<node<T>>> nextLevel;
             for (auto &item:currentLevel) {
                 if (!item) {
-                    std::cout << ' ';
+                    oss << ' ';
                     nextLevel.push_back(std::shared_ptr<node<T>>());
                     nextLevel.push_back(std::shared_ptr<node<T>>());
                 } else {
-                    std::cout << item->val;
+                    oss << item->val;
                     nextLevel.push_back(item->left);
                     nextLevel.push_back(item->right);
                 }
                 for (size_t offset = 0; offset < (std::pow(2, level) - 1) * 2 + 1; ++offset) {
-                    std::cout << ' ';
+                    oss << ' ';
                 }
             }
             --level;
-            std::cout << std::endl;
+            oss << std::endl;
             currentLevel = nextLevel;
         }
+        return oss.str();
     }
 
     std::shared_ptr<node<T>> root;
+
+    //Возможность отключить повороты для тестов
+    bool doTurns = true;
 };
 
+void smallRightTurnTest() {
+/* Например
+             H                         D
+         D       Z                   B   H
+       B   F               ->       A C F Z
+      A C                                             */
+    tree<char> srt;
+    //Без поворота
+    srt.doTurns = false;
+    srt.add('H');
+    srt.add('D');
+    srt.add('B');
+    srt.add('F');
+    srt.add('A');
+    srt.add('C');
+    srt.add('Z');
+    std::string result = srt.print();
+    std::string before = result;
+    result = std::string(result.begin(),
+                         std::remove(result.begin(), result.end(), ' '));//Удаляем пробелы, что-бы было проще сравнивать
+    if (result != "H\nDZ\nBF\nAC\n") {
+        std::cerr << "Pre check fail";
+        exit(EXIT_FAILURE);
+    }
+    //Поворачиваем вручную
+    srt.smallRightTurn(srt.root);
+    result = srt.print();
+    result = std::string(result.begin(), std::remove(result.begin(), result.end(), ' '));
+    if (result != "D\nBH\nACFZ\n") {
+        std::cerr << "Turn check fail";
+        exit(EXIT_FAILURE);
+    }
+    //А теперь - пусть сам вертит!
+    srt = {};
+    srt.add('H');
+    srt.add('D');
+    srt.add('B');
+    srt.add('F');
+    srt.add('A');
+    srt.add('C');
+    srt.add('Z');
+    result = srt.print();
+    result = std::string(result.begin(), std::remove(result.begin(), result.end(), ' '));
+    if (result != "D\nBH\nACFZ\n") {
+        std::cerr << "Auto turn check fail";
+        exit(EXIT_FAILURE);
+    }
+    std::cout << "Small right turn check complete" << std::endl << "Before: " << std::endl << before << "After: "
+              << std::endl << srt.print() << std::endl;
+}
 int main() {
-    std::vector<char> chars;
-    size_t len = 20;
-    for (char c = 'A'; c < 'A' + len; ++c) {
-        chars.push_back(c);
-    }
-    std::random_shuffle(chars.begin(), chars.end());
-    tree<char> tree;
-    for (char c:chars) {
-        std::cout << "Adding " << c << std::endl;
-        tree.add(c);
-        tree.print();
-        std::cout << "----" << std::endl;
-    }
-    std::random_shuffle(chars.begin(), chars.end());
-    for (char c:chars) {
-        std::cout << "Removing " << c << std::endl;
-        tree.del(tree.find(c));
-        tree.print();
-        std::cout << "----" << std::endl;
-    }
+    smallRightTurnTest();
+    /*   std::vector<char> chars;
+       size_t len = 20;
+       for (char c = 'A'; c < 'A' + len; ++c) {
+           chars.push_back(c);
+       }
+       std::random_shuffle(chars.begin(), chars.end());
+       tree<char> tree;
+       for (char c:chars) {
+           std::cout << "Adding " << c << std::endl;
+           tree.add(c);
+           tree.print();
+           std::cout << "----" << std::endl;
+       }
+       std::random_shuffle(chars.begin(), chars.end());
+       for (char c:chars) {
+           std::cout << "Removing " << c << std::endl;
+           tree.del(tree.find(c));
+           tree.print();
+           std::cout << "----" << std::endl;
+       }*/
 
     return 0;
 }
